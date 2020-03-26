2016aba97a2db381bddb404f0f3cdc7b0e19d438
Christian Fries <hello@christian-fries.ch>
2020-03-26 11:23:24 +0100

    Cleanup invalid settings object exception

--- src/Exception/InvalidSettingsObjectException.php
+++ src/Exception/InvalidSettingsObjectException.php
@@ -4,9 +4,9 @@ namespace LST\SettingsBundle\Exception;
 
 class InvalidSettingsObjectException extends Exception
 {
-    public function __construct(string $fqdn)
+    public function __construct(string $className)
     {
-        $message = \Safe\sprintf('The object %s is not a valid settings object. Add the annotation @LST\SettingsBundle\Annotation\Category to the object to use it as a settings category.', $fqdn);
+        $message = sprintf('The object %s is not a valid settings object. Add the annotation @LST\SettingsBundle\Annotation\Category to the object to use it as a settings category.', $className);
         parent::__construct($message, 1585147751);
     }
 }
